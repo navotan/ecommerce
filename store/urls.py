eebfbce3265728765c77da4b7b3ef5b574b7c884
Krzysztof Jaros <krzysztofxjaros@gmail.com>
2020-12-06 20:26:28 +0100

    next

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
     path('product/', views.product, name='product'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('cart/', views.cart_detail, name='card_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
 ]
